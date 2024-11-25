0698d3a75e0d92d9eac18f678022ef3a1098ba45
David Medina <david.medina@sharvik.world>
2024-11-25 20:00:01 +0100

    KAN-3 Implemented Send Billing Data to Service B

--- src/bill-bridge/bill-bridge.controller.ts
+++ src/bill-bridge/bill-bridge.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { ProcessOrderPayloadDTO } from './dtos/process-order-payload.dto';
 import { OrderDTO } from './dtos/order.dto';
 import { BillBridgeService } from './bill-bridge.service';
+import { BillingDTO } from './dtos/billing.dto';
 
 @Controller('bill-bridge')
 export class BillBridgeController {
   constructor(private readonly billBridgeService: BillBridgeService) {}
+
+  @Post()
+  async createBillings(@Body() payload: OrderDTO): Promise<BillingDTO> {
+    return await this.billBridgeService.create(payload);
+  }
 
   @Get()
   async getBillings(
